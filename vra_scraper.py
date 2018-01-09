eca06c8c273072d268dc3a8fa6e8817aa6202972
David E Schober <david.schober@northwestern.edu>
2018-01-09 10:53:23 -0600

    added tests and removed old code

--- vra_scraper.py
+++ vra_scraper.py
@@ -13,68 +13,58 @@ next_gen_fields = os.path.join(script_dir, 'next_gen_field.txt')
 
 def get_list(csv_file):
     """turn a list of items into a, well list. Used for PIDs, vra fields, and headers"""
+
     f = open(csv_file, 'r').read()
     # Note, pop the last one to ensure no errors. 
     # I think I should do a [line for line in lines if line] to scrub
     return [line.strip() for line in f.split('\n') if line]
 
 def get_vra_url(pid):
-    """ Return a vra URL"""
+    """ Return a vra URL. This is hardwired to Images
+    
+    >>> get_vra_url('123')
+    'http://images.northwestern.edu/technical_metadata/123/VRA'
+    
+    """
+
     return "%s%s%s" %("http://images.northwestern.edu/technical_metadata/", pid, "/VRA")
 
-def parse_pid(pid):
-    """ Run pid against list of stuff, lay it out in a list"""
-    try: 
-        images_xpath = get_list(vra_xpath)  
-        url = get_vra_url(pid)
-        namespaces = {"vra":"http://www.vraweb.org/vracore4.htm"}
-        print 'working on ... %s' %url
-        tree = etree.parse(url)
-        tree.getroot()
-        items = [url]
-        for field in images_xpath:
-            items.append(tree.xpath(field, namespaces=namespaces))
-    except Exception as inst:
-        # exception raised, print it to see what went wrong. 
-        items = [url, "something wrong with the xml in this pid, or couldn't get through to the server", inst]
-        
-    return items
+def get_xml_from_file(pid, directory):
+    """Takes a pid, and returns the xmlfile
+    
+    >>> get_xml_from_file('123','/home/david/testxml/')
+    '/home/david/testxml/123.xml'
 
-def iterate_pids(pids):
-    """ Iterate through pidlist return a list of lists"""
-    return [parse_pid(pid) for pid in pids]
-
-def save_csv(list_of_data, outputfile):
-    """ put headers in and save it"""
-    import csv
-    output = open(outputfile, 'wb')
-    writer = csv.writer(output)
-    writer.writerow(["url"]+get_list(next_gen_fields))
-    writer.writerow(["url"]+get_list(vra_xpath))
-    writer.writerow(["url"]+get_list(next_gen_pred))
-
-    for row in list_of_data:
-        writer.writerow(row)
-
-def save_as_mapped_csv(inputfile, outputfile): 
-    save_csv(iterate_pids(get_list(inputfile)), outputfile)
-    print "saved csv file as: %s"%outputfile
+    """
+    xml_file = "%s.xml" %pid.replace(":", "-")
+    file_path = os.path.join(directory, xml_file)
+    return file_path
 
 def save_raw_xml(pids, outputdir):
     """Iterate pids and save xml"""
+
     for pid in get_list(pids):
        print "working on %s" %pid
        filename = pid.replace(":","-")+".xml"
        urllib.URLopener().retrieve(get_vra_url(pid), os.path.join(outputdir, filename)) 
 
-def get_xml_from_file(pid, directory):
-    """Takes a pid, and returns the xmlfile"""
-    xml_file = "%s.xml" %pid.replace(":", "-")
-    data_file = os.path.join(directory, xml_file)
-    return data_file
-
 def flatten_item(url, pid):
-    """ Takes an item's xml and flattens it"""
+    """ Takes an item's xml and flattens it. The PID is used in errors. It returns a key, value tuple 
+    with the key being in the flattened format of '/vra:vra/vra:image/vra:titleSet/vra:display'
+
+    Get a file (or a URL) and flatten it
+    >>> file="samples/test.xml"
+    >>> flattened = flatten_item(file, '123')
+    >>> len(flattened)==80
+    True
+    >>> dictionary_of_data=dict(flattened)
+    >>> dictionary_of_data['/vra:vra/vra:image@id']
+    'inu-dil-258550_w'
+    >>> dictionary_of_data['/vra:vra/vra:image/vra:titleSet/vra:display']
+    'Look-out post in watch tower as above'
+    """
+
+    # Try to parse the URL or XML file.
     try: 
         item = []
         # Parse it and grab the root
@@ -99,7 +89,11 @@ def flatten_item(url, pid):
     return item
 
 def save_as_flat_json(pids, output, **kwargs):
-    """completely flatten the object, and serialize as json"""
+    """completely flatten the object, and serialize as json
+    
+    Takes a list of PIDS, processses them and saves them to a directory.
+    
+    """
 
     items = []
     pid_list = get_list(pids)
@@ -124,25 +118,20 @@ if __name__ == "__main__":
     parser.add_argument('-o', '--outputfile')
     parser.add_argument('-x', '--xmldir')
     parser.add_argument('-r', '--rawxml', action='store_true')
-    parser.add_argument('-j', '--json', action='store_true')
-
-
     parser.add_help
     args = parser.parse_args()
+   
     if args.rawxml and args.outputfile and args.inputfile:
         # Save it as raw xml to a directory
         save_raw_xml(args.inputfile, args.outputfile)
 
-    elif args.inputfile and args.outputfile and args.json and args.xmldir:
+    elif args.inputfile and args.outputfile and args.xmldir:
         save_as_flat_json(args.inputfile, args.outputfile, xmldir=args.xmldir)
 
     elif args.outputfile and args.json and args.inputfile:
-        # Save it as a totally flat JSON file
+        # Save it as a totally flat JSON file. Get the Data from a URL
         save_as_flat_json(args.inputfile, args.outputfile)
     
-    elif args.outputfile and args.inputfile:
-        # Save it as Mapped CSV
-        save_as_mapped_csv(args.inputfile, args.outputfile)
     else:
         parser.print_help()
 
